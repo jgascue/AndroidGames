defce62bb74c92a9c3fd173c6be104cdbd05310e
Javier Gascue <jgascue@gmail.com>
2022-03-23 17:13:51 +0100

    reviews update

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,34 +1,65 @@
 <template>
-    <div class="header">
-        <span class="has-text-centered" href="/">
-            <AppLogo />
-        </span>
-        <nav
-            class="navbar header has-background-success has-shadow is-flex"
-            role="navigation"
-            aria-label="main navigation "
-        >
-            <div class="container is-flex is-align-items-center is-hidden-mobile">
+    <section>
+        <div class="header">
 
-              <div 
-                v-for="(item, key) of items" :key="key"
-                  class="menu-list is-align-items-flex-start "
-                >
-                  <NuxtLink :to="item.to" exact-active-class="is-active" class="has-text-primary">
-                      <b-icon :icon="item.icon" /> {{ item.title }} 
-                  </NuxtLink>
-              </div>
-            </div>
-            <div class="navbar-brand has-text-centered">
-              
-                <div class="navbar-burger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+            <span class="has-text-centered" href="/">
+                <AppLogo />
+            </span>
+
+
+
+            <nav
+                class="navbar has-background-success has-shadow is-flex"
+                role="navigation"
+                aria-label="main navigation "
+            >
+
+                <div class="container is-flex is-align-items-center is-hidden-mobile">
+                    <div 
+                        v-for="(item, key) of items" :key="key"
+                        class="menu-list is-align-items-flex-start "
+                        >
+                        <NuxtLink :to="item.to" exact-active-class="is-active" class="has-text-primary">
+                            <b-icon :icon="item.icon" /> {{ item.title }} 
+                        </NuxtLink>
+                    </div>
                 </div>
-            </div>
-        </nav>
+                <div class="navbar-brand has-text-centered">
+
+                    <div class="navbar-burger"  @click="open = !open">
+                    
+                            <span></span>
+                            <span></span>
+                            <span></span>
+
+
+
+                
+                    </div>
+                    <b-sidebar
+                        v-model="open"
+                        type="is-light"
+                        :fullheight="fullheight"
+                        :fullwidth="fullwidth"
+                        :overlay="overlay"
+                        :right="right"
+                    >
+                        <div class="p-1">
+                            
+                            <b-menu v-for="(item, key) of items" :key="key" class="is-custom-mobile" 
+                            >
+                                <b-menu-list>
+                                    <b-menu-item :icon="item.icon" :label="item.title"></b-menu-item>
+                                </b-menu-list>
+                        
+                            </b-menu>
+
+                        </div>
+                    </b-sidebar>
+                </div>
+            </nav>
     </div>
+        </section>
 </template>
 
 <script>
@@ -42,6 +73,11 @@ export default {
     props: {},
     data() {
         return {
+            open: false,
+            overlay: true,
+            fullheight: true,
+            fullwidth: false,
+            right: false,
             items: [
                 {
                     title: 'Home',
@@ -89,11 +125,20 @@ export default {
 
 <style scoped>
 .header {
-    background-color: black;
+    background-color: rgb(26, 1, 29);
     color: white;
 }
-
+.b-sidebar {
+    position:absolute;
+    top: 230px;
+    width: 250px;
+    z-index: 111; 
+    background: rgb(228, 253, 211);
+}
+.p-1 {
+    padding: 1em;
+}
 .menu-list {
-  line-height: 2.2em;
+    line-height: 2.2em;
 }
 </style>
--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,20 +1,34 @@
 <template>
     <div class="container has-text-centered">
-        <div class="is-size-3 has-text-centered">
+        <div class=" title has-text-centered pt-3 px-2 pb-0 mb-0">
             <strong>
-                <a class="has-text-success-dark" href="/" title="Programs and games Android">PROGRAMS & GAMES & ANDROID </a>
+                <a class="logo has-text-success-dark" href="/" title="Programs and games Android"
+                >
+                PROGRAMS & GAMES ANDROID 
+                </a>
             </strong>
         </div>
-        <p > 
-            <strong class="pa-2 has-text-grey is-size-6">Programs and Games Android and not Android. Play for free Games,
-                discover new programs and find the last Android News
-            </strong>
+        <p class="claim p-3 m-3"> 
+            <span class="has-text-grey-light">
+                Programs and Games Android and not Android. Play for free Games,
+                discover new programs and find the last Android News. <br> Tech, Mobiles, Reviews, News Android.
+            </span>
         </p>
     </div>
 </template>
 
-<style>
+<style lang="scss">
+    .header {
+        background: linear-gradient(45deg, #025223, #010f29,  #015d69);
+    }
     .app-logo {
         height: 180px;
     }
+
+    a.logo {
+       font-family: "Fredoka", sans-serif;
+       font-weight: bold;
+       color: rgb(146, 216, 42) !important;
+    }
+
 </style>

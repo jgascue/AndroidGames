b60724313b8972f8ea59c8169072d5859666106c
Javier Gascue <jgascue@gmail.com>
2022-03-29 16:59:05 +0200

    new test simple page feature

--- pages/reviews/_id.vue
+++ pages/reviews/_id.vue
@@ -1,45 +1,52 @@
 <template>
-    <section class="section reviews">
+    <section class="section">
         <div class="columns">
             <div class="column is-three-quarters">
+       <!--          <pre>
+                    {{ articles }}
+                </pre> -->
+
                 <h1 class="title is-size-2 has-text-black has-text-weight-bold">
                     {{ articles[0].title }}
                 </h1>
-                <h2 class="subtitle is-size-5">
-                    {{ articles[0].subtitle }}
-                </h2>
-
-                <p>
-                    <img :src="articles[1].img" :alt="articles[0].title" />
-                    <!-- <strong> {{ stage.year }} </strong> -->
+                <p
+                    class="
+                        subtitle
+                        is-size-4
+                        has-text-grey-dark has-text-weight-semibold
+                    "
+                >
+                    {{ articles[0].subtitle }} 
                 </p>
 
-                <p v-html="articles[1].body"></p>
-                <p v-html="articles[1].body2"></p>
-                <p v-if="articles[1].body3" v-html="articles[1].body3"></p>
-                <p v-if="articles[1].body4" v-html="articles[1].body4"></p>
-                <p></p>
-                <p></p>
-                <p v-if="articles[1].body5" v-html="articles[1].body5"></p>
-                <p v-if="articles[1].body6" v-html="articles[1].body6"></p>
-                <p v-if="articles[1].body7" v-html="articles[1].body7"></p>
-                <p v-if="articles[1].body8" v-html="articles[1].body8"></p>
-
-                <p></p>
-                <p></p>
-                <p v-if="articles[1].body9" v-html="articles[1].body9"></p>
-                <p v-if="articles[1].body10" v-html="articles[1].body10"></p>
-                <p v-if="articles[1].body11" v-html="articles[1].body11"></p>
-                <p v-if="articles[1].body12" v-html="articles[1].body12"></p>
                 <p>
-                    Next Review:
-                    <a v-if="articles[1].link" :href="articles[1].link">
-                        {{ articles[1].linktext }}</a
-                    >
+                    Category:
+                    <strong> {{ stage.year }} </strong>
                 </p>
+
+                <img class="imageContainer" :src="articles[1].img" :alt="articles[0].title">
+
+                <div class="body-page" v-for="(list, i) in filterlist" :key="i" >
+
+                <!--     <p v-for="(element) in filterlist[i]" :key="element"> -->
+                     <div v-html="list[i]"></div>
+                   <!--  </p> -->
+
+                </div>
+
+                           
+                <div v-if="articles[1].nextTitleLink">
+                    <a :href="articles[1].nextTitleLink">{{ articles[1].nextTitle }}</a>
+                </div>
+                <div v-if="articles[1].nextTitleLink2">
+                    <a :href="articles[1].nextTitleLink2">{{ articles[1].nextTitle2 }}</a>
+                </div>
+
             </div>
-
-            <div class="column is-one-quarters"></div>
+            
+            <div class="column is-one-quarters">
+                ...  
+            </div>
         </div>
     </section>
 </template>
@@ -59,68 +66,74 @@ export default {
 
         try {
             const { data } = await axios.get(
-                `https://www.pcmag.com/reviews/${stage.id}`
+                `https://www.cnet.com/tech/${stage.year}/${id}`
             )
             const html = data
             const $ = cheerio.load(html)
-
             const articles = []
-            const title = $(html).find('h1').text()
+            const paragraf = []
+            const h2titles = []
 
-            const subtitle = $(html).find('p.leading-normal').text()
+            $('.content-header', html).each(function () {
+                const title = $(html).find('h1').text()
+                const subtitle = $(html).find('.c-head_dek').text()
 
-            articles.push({
-                title,
-                subtitle,
-            })
-
-            $('#article', html).each(function () {
-                const titleGame = $(html).find('h2').text()
-                const img = $(html).find('.object-contain').attr('src')
-                const text = $(html).find('.content-body').text()
-                const body = $(html).find('p:first-child').text()
-                const body2 = $(html).find('p:nth-child(2)').text()
-                const body3 = $(html).find('p:nth-child(3)').text()
-                const body4 = $(html).find('p:nth-child(4)').text()
-                const body5 = $(html).find('p:nth-child(5)').text()
-                const body6 = $(html).find('p:nth-child(6)').text()
-                const body7 = $(html).find('p:nth-child(7)').text()
-                const body8 = $(html).find('p:nth-child(8)').text()
-                const body9 = $(html).find('p:nth-child(9)').text()
-                const body10 = $(html).find('p:nth-child(10)').text()
-                const body11 = $(html).find('p:nth-child(11)').text()
-                const body12 = $(html).find('p:nth-child(12)').text()
-                const link = $(html)
-                    .find('.leading-loose:first-child a')
-                    .attr('href')
-                const linktext = $(html)
-                    .find('.leading-loose:first-child a')
-                    .text()
                 articles.push({
-                    titleGame,
-                    text,
-                    img,
-                    body,
-                    body2,
-                    body3,
-                    body4,
-                    body5,
-                    body6,
-                    body7,
-                    body8,
-                    body9,
-                    body10,
-                    body11,
-                    body12,
-                    link,
-                    linktext,
+                    title,
+                    subtitle,
                 })
             })
 
+            $('.article-main-body', html).each(function () {
+                
+                const url = $(html).find('a').attr('href')
+                const img = $('.imageContainer').find('img').attr('src')
+                const time = $(html).find('.assetTime').text()
+                const text = $(html).find('.article-main-body').html()
+                const nextTitle = $(html).find('.speakableTextP1').first().find('a').first().text()
+                const nextTitleLink = $(html).find('.speakableTextP1').first().find('a').first().attr('href')
+                const nextTitle2 = $(html).find('.speakableTextP2').first().find('a').first().text()
+                const nextTitleLink2 = $(html).find('.speakableTextP2').first().find('a').first().attr('href')
+
+                articles.push({
+                    img,  
+                    url,
+                    time,
+                    text,
+                    nextTitle,
+                    nextTitleLink,
+                    nextTitle2,
+                    nextTitleLink2,
+                })
+            })
+
+            const subtitles = []
+            const elements = []
+            const filterlist = []
+
+            $('.article-main-body', html).find('p').siblings().each(function (index, element) {
+                const text = elements.push($(element).text())
+            
+                elements.push(text);
+
+                const filtered = elements.filter(function(el, index) {
+                    return index % 2 === 0;
+                });
+
+                filterlist.push(filtered)
+            })
+
+
+        
             return {
                 data,
                 stage,
                 articles,
+                h2titles,
+                subtitles,
+                elements,
+                filterlist,
+                paragraf,
                 id,
             }
         } catch (err) {
@@ -129,22 +142,33 @@ export default {
     },
     data() {
         return {
+            slug: this.$route.params.slug,
+            AllGames: [],
             articles: [],
         }
     },
     head() {
         return {
-            title: `Android Games: ${this.articles[0].title}. - Programs & Games Android`,
+            title: 'Programs Games Android' + ' | ' + this.articles[0].title ,
             meta: [
                 {
-                    hid: `Android Games: ${this.articles[0].title}`,
-                    name: `Android Games: ${this.articles[0].title}`,
-                    content: `Android Games: ${this.articles[0].title}`,
+                    hid: this.articles[0].title,
+                    name: this.articles[0].subtitle,
+                    content: this.articles[0].title,
                 },
             ],
         }
     },
+    
+    computed: { 
+
+    },
+    mounted () { 
+        const imatges = this.$el.querySelector('img');
+        imatges.classList.add('imatges-cool');
+    }
 }
+
 </script>
 
 <style>
@@ -162,10 +186,23 @@ p {
     margin: 10px 0;
 }
 
+div {
+    margin-bottom:20px;
+}
 h1,
 h2,
 h3 {
     font-weight: bold;
     font-family: 'Fredoka', sans-serif;
+}
+
+.body-page img {
+    display: block;
+    height: 355px !important;
+    width: auto !important;
+    border-radius: 10px 30px;
+    float: left;
+    margin-right: 40px;
+    margin-bottom: 20px;
 }
 </style>
